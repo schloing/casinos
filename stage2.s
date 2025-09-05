8f59e3259997f9672e5eaa6979d47fd7ba7a6704
schloing <user.datagram.protocol@proton.me>
2025-09-05 19:27:53 +1000

    convert fasm to nasm format

--- stage2.s
+++ stage2.s
@@ -1,8 +1,7 @@
-    format elf              ; elf32
-    org 0x10000
-    section '.text' writeable executable
-    use32
+    bits 32
+    section .text
+
 _start:
-    extrn main_32
+    extern main_32
     call main_32
     jmp $
