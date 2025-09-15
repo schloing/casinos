fb735548788646db0617f6ccc7c079a4bb297f6a
schloing <user.datagram.protocol@proton.me>
2025-09-15 19:35:10 +1000

    remove debug junk code

--- vbe.c
+++ vbe.c
@@ -6,16 +6,16 @@
 int vbe_controller_get_info(struct vbe_info_structure* vbe_info)
 {
     struct rm_regs r = { 0 };
-    r.eax = 0x4f00;
-    r.edi = (uint32_t)vbe_info;
-    rm_int(0x10, &r, &r);
 
-    printl(vbe_info->signature);
+    r.eax = 0x4f00;
+    r.edi = 0;
+    r.esi = (uint32_t)vbe_info;
+
+    rm_int(0x10, &r, &r);
 
     if ((r.eax & 0x00ff) != 0x4f) {
         return -1;
     }
 
     return 0;
-//  if (vbe_info->
 }
