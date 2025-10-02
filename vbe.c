1a6e36cc49db35b479a9ba6abe7399ab71c63d48
schloing <user.datagram.protocol@proton.me>
2025-10-03 00:15:19 +1000

    vbe get info

--- vbe.c
+++ vbe.c
@@ -8,8 +8,26 @@ int vbe_controller_get_info(struct vbe_info_structure* vbe_info)
     struct rm_regs r = { 0 };
 
     r.eax = 0x4f00;
+    r.es = RM_SEG(vbe_info);
+    r.edi = RM_OFF(vbe_info);
+
+    rm_int(0x10, &r, &r);
+
+    if ((r.eax & 0x00ff) != 0x4f) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int vbe_get_mode_info(int mode_no, struct vbe_mode_info_structure* vbe_mode_info)
+{
+    struct rm_regs r = { 0 };
+
+    r.eax = 0x4f01;
+    r.ecx = mode_no;
     r.edi = 0;
-    r.esi = (uint32_t)vbe_info;
+    r.esi = (uint32_t)vbe_mode_info;
 
     rm_int(0x10, &r, &r);
 
