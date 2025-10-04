816dca3cf66367f5ad0dc13a3c78c3c6979af317
schloing <user.datagram.protocol@proton.me>
2025-10-04 18:29:41 +1000

    segmentation utility functions

--- real.h
+++ real.h
@@ -20,7 +20,12 @@ struct rm_regs {
 
 void rm_int(uint8_t int_no, struct rm_regs* in_regs, struct rm_regs* out_regs);
 
-#define RM_OFF(x) (uint16_t)((uint32_t)x & 0x0f)
-#define RM_SEG(x) (uint16_t)((uint32_t)x >> 4)
+#define RM_SEG(x) ((uint16_t)((uint32_t)x >> 4))
+#define RM_OFF(x) ((uint16_t)((uint32_t)x & 0x0f))
+
+#define RM_FAR_SEG(x) ((uint16_t)((uint32_t)x >> 16))
+#define RM_FAR_OFF(x) ((uint16_t)((uint32_t)x & 0xffff))
+
+#define RM_FAR_TO_PHYS(x) (((uint32_t)RM_FAR_SEG(x) << 4) + (uint32_t)RM_FAR_OFF(x))
 
 #endif
