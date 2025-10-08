003a0e5c67b15c9fa236c1b5a1b4bfa25cbc817e
schloing <user.datagram.protocol@proton.me>
2025-10-08 19:31:18 +1100

    typedefs for compatibility with printf implementation

--- include/stdint.h
+++ include/stdint.h
@@ -1,6 +1,8 @@
 #ifndef STDINT_H
 #define STDINT_H
 
+#define NULL ((void*)0) // FIXME: not supposed to be here
+
 typedef signed char        int8_t;
 typedef unsigned char      uint8_t;
 typedef short              int16_t;
@@ -9,5 +11,7 @@ typedef int                int32_t;
 typedef unsigned int       uint32_t;
 typedef long long          int64_t;
 typedef unsigned long long uint64_t;
+typedef uint32_t           uintptr_t;
+typedef uint64_t           intmax_t ;
 
 #endif
